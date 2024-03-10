352234ca876718aaa9aa4c2a5b800a01928bf290
TechGeno <harshasri910@gmail.com>
2024-03-10 19:11:37 +0530

    New Nav- Bug

--- js/script.js
+++ js/script.js
@@ -35,4 +35,9 @@ $(document).ready(function () {
         });
         typingIndex = (typingIndex + 1) % typingTexts.length;
     }, 2000);
+
+    // Top toggle
+    document.querySelector('.name').addEventListener('click', function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to the top of the page when the name is clicked
+    });
 });
